01afae010bf03f3e72282590b5581b40506ac547
Satria Persada <triasada@yahoo.com>
2025-03-28 13:28:21 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ export default {
   maxListeners: 15,
   customUserDataDir: './userDataDir/',
   webhook: {
-    url: null,
+    url: 'https://efd4-110-138-220-226.ngrok-free.app/api/wa/webhook',
     autoDownload: true,
     uploadS3: false,
     readMessage: true,
